e06bc4c3cb376ae3bba3e78d060a4248fc5e8da2
Vladimir <volodyawork@gmail.com>
2015-03-07 11:06:32 +0200

    MySQL stored procedure

--- MySQL/stored_procedure.sql
+++ MySQL/stored_procedure.sql
@@ -1,10 +1,25 @@
+USE var;
 DELIMITER |
 DROP PROCEDURE IF EXISTS super_procedure|
-CREATE PROCEDURE super_procedure ()
-BEGIN
-    SELECT 'Hello World!';
-END|
+CREATE PROCEDURE super_procedure (IN p1 VARCHAR(256))
+    BEGIN
+        SELECT CONCAT(p1, ' HELLO');
+    END|
 
 DELIMITER ;
 
-CALL super_procedure();
+CALL super_procedure('1234');
+
+#====================
+USE var;
+DELIMITER |
+DROP PROCEDURE IF EXISTS super_procedure_OUT|
+CREATE PROCEDURE super_procedure_OUT (OUT res VARCHAR(256))
+    BEGIN
+        SET res = 'RESULT';
+    END|
+
+DELIMITER ;
+
+CALL super_procedure_OUT(@a);
+SELECT @a;
